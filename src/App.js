6f3d3176397a96f5a9216bd1f56e066032772825
Tai-Huynh-1 <93554992+Tai-Huynh-1@users.noreply.github.com>
2022-06-09 16:36:30 -0700

    finished my about-us page

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - Modified
+          Learn React - Modified - Finished My Feature
         </a>
       </header>
     </div>
