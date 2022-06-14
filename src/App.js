068c17487bd953a69d2327da1e26b3ee2ab3a8ef
yeoja-v <99926444+yeoja-v@users.noreply.github.com>
2022-06-13 17:09:27 -0700

    refactor code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import Link from './components/Link'
+
+
 
 function App() {
   return (
@@ -9,14 +12,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Link />
+        <Link />
+        <Link />
+        <Link />
+        <Link />
       </header>
     </div>
   );
---
+++ src/components/Link.js
@@ -0,0 +1,16 @@
+
+
+function Link() {
+    return (
+      <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+    )
+  }
+
+  export default Link
