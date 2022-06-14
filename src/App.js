4cb6bc6a0f069ac3becd1a068d9052b74f427042
Tai-Huynh-1 <93554992+Tai-Huynh-1@users.noreply.github.com>
2022-06-13 17:24:10 -0700

    built Link component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Link from "./components/Link"
+
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -9,17 +12,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React - Modified - Finished My Feature
-        </a>
+        <Link />
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
+
+
---
+++ src/components/Link.js
@@ -0,0 +1,14 @@
+function Link() {
+    return (
+        <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React - Modified - Finished My Feature
+        </a>
+    )
+}
+
+export default Link
