b0eee127dc57aba461d65de3d7858ee3a795a6e4
Tai-Huynh-1 <93554992+Tai-Huynh-1@users.noreply.github.com>
2022-06-22 17:04:23 -0700

    went over promises and started useEffect - tmrw talk about lifecycle and dependency array

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -3,15 +3,56 @@ import axios from 'axios'
 
 function User() {
     const [users, setUsers] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    // promises using Async/await - with arrow function
+    const getUsers = async () => {
+        try {
+            const result = await axios.get("https://jsonplaceholder.typicode.com/users")
+            setUsers(result.data)
+            setLoading(false)
+        } catch (error) {
+            console.log(error)
+            setLoading(false)
+        }
+    }
+
+    // writing Async/await with normal function
+    // async function getUsersNormalFunc () {
+    //     try {
+    //         const result = await axios.get("https://jsonplaceholder.typicode.com/users")
+    //         setUsers(result.data)
+    //         setLoading(false)
+    //     } catch (error) {
+    //         console.log(error)
+    //         setLoading(false)
+    //     }
+    // }
 
     useEffect(() => {
-        axios.get("https://jsonplaceholder.typicode.com/users")
-        .then(result => setUsers(result.data))
+        // handle promises with .then() & .catch() methods
+        // axios.get("https://jsonplaceholder.typicode.com/users")
+        // .then(result => setUsers(result.data))
+        // .catch(error => console.log(error))
+        console.log("making network request")
+        getUsers()
+        // getUsersNormalFunc()
+
     } , [])
 
-    console.log("users from API: ", users)
+    // useEffect(() => {}, [])
 
-    return <div>User</div>
+    // console.log("users from API: ", users)
+
+    console.log("rendering my component")
+
+    if (loading) {
+        return <div>LOADING...</div>
+    }
+
+    return <ul>{users.map(user => {
+        return (<li key={user.id}>{user.name}</li>)
+    })}</ul>
 }
 
 export default User
