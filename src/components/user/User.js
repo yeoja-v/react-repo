9220b52b35d8a39874bdca6cc885bd1757dbf87d
Tai-Huynh-1 <93554992+Tai-Huynh-1@users.noreply.github.com>
2022-06-23 17:01:21 -0700

    useEffect dependency array

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -3,6 +3,8 @@ import axios from 'axios'
 
 function User() {
     const [users, setUsers] = useState([])
+    const [user, setUser] = useState(null)
+    const [userId, setUserId] = useState(0)
     const [loading, setLoading] = useState(true)
 
     // promises using Async/await - with arrow function
@@ -17,42 +19,41 @@ function User() {
         }
     }
 
-    // writing Async/await with normal function
-    // async function getUsersNormalFunc () {
-    //     try {
-    //         const result = await axios.get("https://jsonplaceholder.typicode.com/users")
-    //         setUsers(result.data)
-    //         setLoading(false)
-    //     } catch (error) {
-    //         console.log(error)
-    //         setLoading(false)
-    //     }
-    // }
+    async function getUser (userId) {
+        try {
+            const result = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`)
+            setUser(result.data)
+            setLoading(false)
+        } catch (error) {
+            console.log(error)
+            setLoading(false)
+        }
+    }
 
     useEffect(() => {
         // handle promises with .then() & .catch() methods
         // axios.get("https://jsonplaceholder.typicode.com/users")
         // .then(result => setUsers(result.data))
         // .catch(error => console.log(error))
-        console.log("making network request")
-        getUsers()
-        // getUsersNormalFunc()
 
-    } , [])
+        getUser(userId)
 
-    // useEffect(() => {}, [])
+    }, [userId])
 
-    // console.log("users from API: ", users)
 
-    console.log("rendering my component")
+    console.log("user id", userId)
+    console.log('user', user)
+
 
     if (loading) {
         return <div>LOADING...</div>
     }
 
-    return <ul>{users.map(user => {
-        return (<li key={user.id}>{user.name}</li>)
-    })}</ul>
+    return <div>
+        <input className='border border-black' type="text" value={userId} onChange={(event) => setUserId(event.target.value)} />
+
+        <h1>u</h1>
+    </div>
 }
 
 export default User
