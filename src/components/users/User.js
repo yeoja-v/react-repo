4f190fa56f2a3e94db5626084f4b973fd5912821
yeoja-v <99926444+yeoja-v@users.noreply.github.com>
2022-06-23 17:03:06 -0700

    Jun 23

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -3,14 +3,23 @@ import { useEffect, useState} from 'react'
 import axios from 'axios'
 
 function User() {
-    const [users, setUsers] = useState([])
+    const [userId, setUserId] = useState(0)
+    const [user, setUser] = useState(null)
+    const [users, setUsers] = useState(null)
+    const [loading, setLoading] = useState(true)
 
-    const getUser = async () => {
-      const result = await axios.get("https://jsonplaceholder.typicode.com/users")
-      setUsers(result.data)
-      // PROMISES using Async/await
-      // works exactly the same as .then and .catch !
+    async function getUser (userId) {
+      try { 
+        const result = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`)
+      } catch (error) {
+        console.log(error)
+        setLoading(false)
+      }
     }
+
+          // PROMISES using Async/await
+      // works exactly the same as .then and .catch !
+
     //  catch (error) {
     //   console.log(error)
     // }
@@ -24,7 +33,6 @@ function User() {
     //   }
     // }
 
-
     // axios method is GET, to get resources from JSONPLACEHOLDER
     // if delete users 15.. , replace GET with delete https://jsonplaceholder.typicode.com/users/15
   
@@ -33,12 +41,12 @@ function User() {
         // .then(result => setUsers(result.data))
         // .catch(error => console.log(error))
         // getUserNormalFunc()
-        console.log("making network request")
 
 
-        getUser()
-    } , [])
-    console.log("rendering my component")
+        getUser(userId) //fetches user and updates state, triggers re-render
+    } , [userId])
+    console.log("userId", userId)
+    console.log("user", user)
 
     // // get method, returns a promise, then is a method we can chain on to a promise to handle that promise once a value has been fulfilled
 // 
@@ -47,12 +55,20 @@ function User() {
     return <div>LOADING...</div>
   }
   
-  return ( <ul>
-    {users.map(user => {
-      return (<li key={user.id}>{user.name}</li>)
-    })}
-  </ul>
-  )
+  return <div>
+    <input className='border border-black' type="text" value={userId} onChange={(event) => setUserId(event.target.value)} />
+    <h1>
+
+    </h1>
+  </div>
+
+  // return ( <ul>
+  //   {users.map(user => {
+  //     return (<li key={user.id}>{user.name}</li>)
+  //   })}
+  // </ul>
+  // )
 }
 
 export default User
+
